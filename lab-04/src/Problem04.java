aebf31ee12c8a282eadc16a7d7d455b07f61fce3
Adil Sulaymankulov <sulaymankulov_a@auca.kg>
2022-04-06 23:46:01 +0600

    Finish problem02 from lab-04

---
+++ lab-04/src/Problem04.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Problem04 {
+    public static void main(String[] args) {
+        final int ELEMENTS = 100_000;
+        {
+            var nums = new ArrayList<Integer>();
+
+            long startTime = System.nanoTime();
+            for (int i = 0; i < ELEMENTS; ++i) {
+                nums.add((int) (Math.random() * 100.0));
+            }
+//                for (int i = 0; i < ELEMENTS; ++i) {
+//                    nums.add(0, (int) (Math.random() * 100.0));
+//                }
+//                for (int i = 0; i < ELEMENTS * 2; ++i) {
+//                    nums.remove(0);
+//                }
+
+            long endTime = System.nanoTime();
+            long time =  endTime - startTime;
+            System.out.printf("%.5f%n", time  / 1_000_000_000.0);
+        }
+
+        {
+            var nums = new ArrayListInt();
+
+            long startTime = System.nanoTime();
+            for (int i = 0; i < ELEMENTS; ++i) {
+                nums.add((int) (Math.random() * 100.0));
+            }
+//            for (int i = 0; i < ELEMENTS; ++i) {
+//                nums.add(0, (int) (Math.random() * 100.0));
+//            }
+//            for (int i = 0; i < ELEMENTS * 2; ++i) {
+//                nums.remove(0);
+//            }
+            long endTime = System.nanoTime();
+            long time = endTime - startTime;
+            System.out.printf("%.5f%n", time / 1_000_000_000.0);
+        }
+    }
+}
